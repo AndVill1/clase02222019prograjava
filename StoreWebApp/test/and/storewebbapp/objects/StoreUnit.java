33267205b0003002d413f3a183a74ed875346b28
AndVill1 <wanderingsatellite0@gmail.com>
2019-02-22 20:30:19 -0600

    Create StoreUnit.java

---
+++ StoreWebApp/test/and/storewebbapp/objects/StoreUnit.java
@@ -0,0 +1,47 @@
+
+package and.storewebbapp.objects;
+
+public class StoreUnit 
+{
+    private String m_strName;
+    private int m_iPrice;
+    private int m_iQuantity;
+
+    public String getName() 
+    {
+        return m_strName;
+    }
+
+    public void setName(String p_strName) 
+    {
+        m_strName = p_strName;
+    }
+
+    public int getPrice() 
+    {
+        return m_iPrice;
+    }
+
+    public void setPrice(int p_iPrice) 
+    {
+        m_iPrice = p_iPrice;
+    }
+
+    public int getQuantity() 
+    {
+        return m_iQuantity;
+    }
+
+    public void setQuantity(int p_iQuantity) 
+    {
+        m_iQuantity = p_iQuantity;
+    }
+    
+    public int getCost()
+    {
+        int iPrice = getPrice();
+        int iQuantity = getQuantity();
+        int result = iPrice * iQuantity;
+        return result;
+    }
+}
